62dc5f1d3dad987cf96c694d476d09ba83deeb98
GuilhermeMaia23 <gumstere@gmail.com>
2021-11-20 14:49:44 -0300

    Inclusao de Icones para as Tab

--- src/Navegation/Tab.js
+++ src/Navegation/Tab.js
@@ -3,7 +3,7 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Goals from '../Views/Goals';
 import Market from '../Views/Market';
 import Home from '../Views/Home';
-import Icon from 'react-native-vector-icons'
+import Ionicons from 'react-native-vector-icons/Feather'
 
 
 const Tab = createBottomTabNavigator();
@@ -11,36 +11,42 @@ const Tab = createBottomTabNavigator();
 
 export default function Tabs(props) {
   return (
-    <Tab.Navigator screenOptions={({route}) => ({
+    <Tab.Navigator screenOptions={({ route }) => ({
 
-      // // tabBarIcon: ({ focused, color, size }) => {
-      // //   let iconName;
+      tabBarIcon: ({ focused, color, size }) => {
+        let iconName;
 
-      // //   if (route.name === 'Home') {
-      // //     iconName = focused
-      // //       ? 'ios-information-circle'
-      // //       : 'ios-information-circle-outline';
-      // //   } else if (route.name === 'Settings') {
-      // //     iconName = focused ? 'ios-list-box' : 'ios-list';
-      // //   }
-
-      // //   // You can return any component that you like here!
-      //   return <Icon name={iconName} size={size} color={color} />;
-      // },
+        if (route.name === 'Home') {
+          iconName = focused
+            ? 'home'
+            : 'home';
+        } else if (route.name === 'Goals') {
+          iconName = focused ? 'target' : 'target';
+        } else if (route.name === 'Market') {
+          iconName = focused ? 'bar-chart' : 'bar-chart';
+        }
+        return <Ionicons name={iconName} size={size} color={color} />;
+      },
 
 
       tabBarActiveTintColor: '#FF7F50',
       tabBarInactiveTintColor: '#2B37FF',
     })}>
-      <Tab.Screen name="Home" component={Home} options={{title: 'Página Principal', headerStyle: {backgroundColor: '#0000FF'}, headerTintColor: '#EEF4FF', headerTitleStyle: {
-        fontWeight: 'bold',
-      }}}/>
-      <Tab.Screen name="Goals" component={Goals} options={{title: 'Metas', headerStyle: {backgroundColor: '#0000FF'}, headerTintColor: '#EEF4FF', headerTitleStyle: {
-        fontWeight: 'bold',
-      }}}/>
-      <Tab.Screen name="Mercado" component={Market} options={{title: 'Mercado', headerStyle: {backgroundColor: '#0000FF'}, headerTintColor: '#EEF4FF', headerTitleStyle: {
-        fontWeight: 'bold',
-      }}} />
+      <Tab.Screen name="Home" component={Home} options={{
+        title: 'Página Principal', headerStyle: { backgroundColor: '#0000FF' }, headerTintColor: '#EEF4FF', headerTitleStyle: {
+          fontWeight: 'bold',
+        }
+      }} />
+      <Tab.Screen name="Goals" component={Goals} options={{
+        title: 'Metas', headerStyle: { backgroundColor: '#0000FF' }, headerTintColor: '#EEF4FF', headerTitleStyle: {
+          fontWeight: 'bold',
+        }
+      }} />
+      <Tab.Screen name="Market" component={Market} options={{
+        title: 'Mercado', headerStyle: { backgroundColor: '#0000FF' }, headerTintColor: '#EEF4FF', headerTitleStyle: {
+          fontWeight: 'bold',
+        }
+      }} />
     </Tab.Navigator>
   )
 }
